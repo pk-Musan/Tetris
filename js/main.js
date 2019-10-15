b4518f741dcb76ac9051786e75b6902f70675834
pk-Musan <mu.1ok.san@gmail.com>
2019-10-15 18:52:00 +0900

    fix: main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,59 @@
 enchant();
 
+let core;
+let TITLE = 0;
+let MAIN = 1;
+let scene_flag = TITLE;
+
 window.onload = function() {
-    let game = new Game(320, 320)
-    game.fps = 24;
-    game.preload('../image/chara1.png');
+    init();
 
-    game.onload = function() {
+    core.onload = function() {
         let kuma = new Sprite(32, 32);
+        let x_speed = 0;
+        let y_speed = 0;
 
-        kuma.image = game.assets['../image/chara1.png'];
+        kuma.image = core.assets['../image/chara1.png'];
         kuma.x = 100;
         kuma.y = 120;
 
-        game.rootScene.addChild(kuma);
-        game.rootScene.backgroundColor = '#7ecef4';
+        core.rootScene.addChild(kuma);
+        core.rootScene.backgroundColor = '#7ecef4';
+
+        kuma.on('enterframe', function() {
+           
+        })
+
     }
 
-    game.start();
+    core.start();
 };
+
+function init() {
+    core = new Core(320, 640);
+    core.fps = 24;
+
+}
+
+function gameloop() {
+    core.onload = function() {
+        switch(scene_flag) {
+            case TITLE:
+                title();
+                break;
+            case MAIN:
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+function title() {
+    // タイトル用の文字とか用意する
+    let titlename = new Label();
+    titlename.text = 'TETRIS';
+
+    core.rootScene.addChild(titlename);
+}
 
